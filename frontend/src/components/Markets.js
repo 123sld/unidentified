f2a3caccadabeb25e309be5277f087c16e726b1a
Ivan Malison <IvanMalison@gmail.com>
2022-10-24 23:42:16 -0600

    Show outcome prices

--- frontend/src/components/Markets.js
+++ frontend/src/components/Markets.js
@@ -8,14 +8,14 @@ var markets = [
     "category": "Politics",
     "outcomes": [
       {
-        "name": "R House, R Senate",
-        "yes_price": 61,
-        "no_price": 39
-      },
-      {
         "name": "R House, D Senate",
         "yes_price": 37,
         "no_price": 63
+      },
+      {
+        "name": "R House, R Senate",
+        "yes_price": 61,
+        "no_price": 39
       },
       {
         "name": "D House, R Senate",
@@ -52,7 +52,7 @@ var markets = [
     "category": "Sports",
     "outcomes": [
       {
-        "name": "Washington Capitals Win",
+        "name": "Washington Capitals Win really really really really really long outcome",
         "yes_price": 50,
         "no_price": 50
       },
@@ -70,7 +70,7 @@ var markets = [
     "outcomes": [
       {
         "name": "Colorado Avalanche Win",
-        "yes_price": 50,
+        "yes_price": 40,
         "no_price": 50
       },
       {
@@ -256,17 +256,28 @@ class Markets extends Component {
       return (
         <>
           <div>
-            <Grid container direction="column" padding={3} alignItems="center">
-              <Box sx={{ display: 'flex', flexWrap: 'wrap', }} alignItems="stretch" padding={1}>
+            <Grid container direction="column" padding={3} alignItems="center" justifyContent="center">
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', maxWidth: 1600}} justifyContent="center" padding={1} alignItems="center">
                 {
                   markets.map((market, index) => (
-                    <Paper elevation={2} sx={{ m: .5}}>
-                      <Card sx={{ minHeight: 200, maxHeight: 200, minWidth: 300, maxWidth: 300 }} variant="outlined">
+                    <Paper elevation={2} sx={{ m: .5}} alignItems="center">
+                      <Card sx={{ width: 350 }} variant="outlined">
                         <CardContent>
-                          <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
-                            {market.category}
-                          </Typography>
-                          <Typography noWrap variant="h6"> {market.name} </Typography>
+                          <Typography noWrap variant="h6" sx={{ fontSize: 12 }} gutterBottom> { market.name } </Typography>
+                          {
+                            market.outcomes.sort((a, b) => {
+                              return b.yes_price - a.yes_price;
+                            }).slice(0, 2).map((outcome, index) => (
+                              <>
+                                <Typography sx={{ fontSize: 12 }} noWrap gutterBottom>
+                                  {outcome.name}
+                                </Typography>
+                                <Typography variant="h5">
+                                  { outcome.yes_price }
+                                </Typography>
+                              </>
+                            ))
+                          }
                         </CardContent>
                       </Card>
                     </Paper>
